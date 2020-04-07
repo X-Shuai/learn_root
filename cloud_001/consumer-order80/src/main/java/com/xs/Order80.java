03e0717431eb7ae56f176ac4fa55aee7f321d576
xs-shuai <xs-shuai@qq.com>
2020-04-08 00:50:49 +0800

    单机搭建完成

--- cloud_001/consumer-order80/src/main/java/com/xs/Order80.java
+++ cloud_001/consumer-order80/src/main/java/com/xs/Order80.java
@@ -2,6 +2,7 @@ package com.xs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: learn_root
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2020-04-06 21:35
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class Order80 {
     public static void main(String[] args) {
         SpringApplication.run(Order80.class,args);
