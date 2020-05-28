aff6ddb105486c80a28c898579be72218483eb85
xs-shuai <xs-shuai@qq.com>
2020-05-29 00:46:19 +0800

    gateway

---
+++ cloud_my/gateway/src/main/java/com/xs/GateWay9001.java
@@ -0,0 +1,17 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learn_root
+ * @description: 启动类
+ * @author: xs-shuai.com
+ * @create: 2020-05-28 22:34
+ **/
+@SpringBootApplication
+public class GateWay9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWay9001.class,args);
+    }
+}
