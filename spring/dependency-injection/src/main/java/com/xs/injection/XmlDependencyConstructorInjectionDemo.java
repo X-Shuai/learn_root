c791ee9567ab876dec295f949b6867da6c0a70ae
X-Shuai <xs-shuai@qq.com>
2020-11-14 11:15:46 +0800

    spring 依赖注入

---
+++ spring/dependency-injection/src/main/java/com/xs/injection/XmlDependencyConstructorInjectionDemo.java
@@ -0,0 +1,26 @@
+package com.xs.injection;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * @program: learn_root
+ * @description: xml 构造器 注入
+ * @author: xs-shuai.com
+ * @create: 2020-11-14 10:24
+ **/
+public class XmlDependencyConstructorInjectionDemo {
+
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+
+        String xmlResourcePath = "classpath:/META-INF/autowiring-dependency-constructor-injection.xml";
+        // 加载 XML 资源，解析并且生成 BeanDefinition
+        beanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+        // 依赖查找并且创建 Bean
+        UserHolder userHolder = beanFactory.getBean(UserHolder.class);
+        System.out.println(userHolder);
+    }
+}
