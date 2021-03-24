8dc8ae7b04066b46a9b1cda93596f75e9541dead
xs-shuai <xs-shuai@qq.com>
2021-03-25 00:12:09 +0800

    rabbit Mq

---
+++ other/rabbit-mq/src/main/java/com/xs/RabbitMqApplication.java
@@ -0,0 +1,17 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: learn_root
+ * @description:
+ * @author: xs-shuai.com
+ * @create: 2021-03-24 23:29
+ **/
+@SpringBootApplication
+public class RabbitMqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqApplication.class,args);
+    }
+}
---
+++ other/rabbit-mq/src/test/java/com/xs/RabbitMqTest.java
@@ -0,0 +1,36 @@
+package com.xs;
+
+import com.xs.config.RabbitmqConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: learn_root
+ * @description:
+ * @author: xs-shuai.com
+ * @create: 2021-03-24 23:41
+ **/
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class RabbitMqTest {
+
+    @Resource
+    RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSendByTopics(){
+        for (int i=0;i<5;i++){
+            String message = "sms email inform to user"+i;
+            rabbitTemplate.convertAndSend(RabbitmqConfig.EXCHANGE_TOPICS_INFORM,"inform.sms.email",message);
+            System.out.println("Send Message is:'" + message + "'");
+        }
+    }
+
+
+}
