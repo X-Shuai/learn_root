d4a24a053eab0131216da48bfb5056f8eebc6bdd
lishuai <lishuai@teleware.cn>
2021-07-29 18:03:35 +0800

    2021-7-29 计划1

--- leetcode/src/main/java/com/xs/y2021/m7/Solution1.java
+++ leetcode/src/main/java/com/xs/y2021/m7/Solution1.java
@@ -21,41 +21,40 @@ package com.xs.y2021.m7;
  * @author: xs-shuai.com
  * @create: 2021-07-29 17:14
  **/
-public class Solution {
+public class Solution1 {
         public int search(int[] nums, int target) {
-
             int start= 0;
             int end= nums.length-1;
-            int middle= (start+end)/2;
+            int middle= start + (end - start) / 2;
            return find(nums, target, start, end, middle);
         }
 
     private int find(int[] nums, int target, int start, int end, int middle) {
-        if (start ==middle||middle==end){
-            return -1;
-        }
         if (nums[middle]== target){
             return middle;
         }
+        if (start>=end){
+            return -1;
+        }
         if (nums[middle]> target){
             //
-            end=middle;
-            middle=(start+end)/2;
+            end=middle-1;
+            middle=start + (end - start) / 2;
             return  find(nums,target,start,end,middle);
         }
         if (nums[middle]< target){
-            start=middle;
-            middle=(start+end)/2;
+            start=middle+1;
+            middle=start + (end - start) / 2;
             return  find(nums,target,start,end,middle);
         }
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {-1,0,3,5,9,12};
-                int target = 9;
-        Solution solution=new Solution();
-        int search = solution.search(nums, target);
-        System.out.println(search);
-    }
+//    public static void main(String[] args) {
+//        int[] nums = {2,5};
+//        int target = 5;
+//        Solution1 solution=new Solution1();
+//        int search = solution.search(nums, target);
+//        System.out.println(search);
+//    }
 }
